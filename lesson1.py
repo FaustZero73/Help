e034ae3078b4f1b5934abfe9274c5a67cf9a40d1
FaustZero73 <kulakova612@gmail.com>
2024-10-18 13:32:25 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+# name = input('Введите Ваше имя: ')
+# # current_year = 2024
+# date_of_birth = input('В каком году Вы родились? ')
+# age = current_year - int(date_of_birth)
+# print('Привет, ',name)
+# print('В этом году Вам',age, 'лет')
+from dataclasses import replace
+
+# print('Как дела?'.upper())
+# print('КаК Дела?'.lower())
+# print('Привет, ','name'.replace(__old: 'Привет, ', __new: 'Пока, '))
+print('Как дела?.replace(__old: ' ', __new: ''))
